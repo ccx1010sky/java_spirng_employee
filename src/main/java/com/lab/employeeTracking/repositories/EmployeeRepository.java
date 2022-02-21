aa70d88bd82f416005e6a4d297e42f8d891968b0
ccx1010sky <ccx1010@gmail.com>
2022-02-21 14:53:54 +0000

    employeeRepository extends JpaRepository<Employee,Long>

--- src/main/java/com/lab/employeeTracking/repositories/EmployeeRepository.java
+++ src/main/java/com/lab/employeeTracking/repositories/EmployeeRepository.java
@@ -1,4 +1,7 @@
 package com.lab.employeeTracking.repositories;
 
-public interface EmployeeRepository {
+import com.lab.employeeTracking.models.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee,Long> {
 }

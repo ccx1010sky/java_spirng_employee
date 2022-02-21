d74b87cfdbca0195b530f483b4030b593d5eaca8
ccx1010sky <ccx1010@gmail.com>
2022-02-21 14:43:26 +0000

    @Entity @Table @Id @GeneratedValue @column

--- src/main/java/com/lab/employeeTracking/models/Employee.java
+++ src/main/java/com/lab/employeeTracking/models/Employee.java
@@ -1,10 +1,59 @@
 package com.lab.employeeTracking.models;
 
+import javax.persistence.*;
+
+
+@Entity
+@Table(name="employees")
+
 public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
 
     private String name;
     private int age;
     private int employeeNumber;
     private String email;
 
+    public Employee(String name, int age, int employeeNumber, String email) {
+        this.name = name;
+        this.age = age;
+        this.employeeNumber = employeeNumber;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getEmployeeNumber() {
+        return employeeNumber;
+    }
+
+    public void setEmployeeNumber(int employeeNumber) {
+        this.employeeNumber = employeeNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }

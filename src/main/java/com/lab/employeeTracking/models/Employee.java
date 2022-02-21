36466dc44ceefd73f3d25080fcf85b948402fe7c
ccx1010sky <ccx1010@gmail.com>
2022-02-21 14:37:39 +0000

    first commit

---
+++ src/main/java/com/lab/employeeTracking/models/Employee.java
@@ -0,0 +1,10 @@
+package com.lab.employeeTracking.models;
+
+public class Employee {
+
+    private String name;
+    private int age;
+    private int employeeNumber;
+    private String email;
+
+}

738909bd7eaed1aec4396f87c67d1fe988934373
ccx1010sky <ccx1010@gmail.com>
2022-02-22 10:59:33 +0000

    modified Employee and EmployeeTrackingApplications

--- src/main/java/com/lab/employeeTracking/models/Employee.java
+++ src/main/java/com/lab/employeeTracking/models/Employee.java
@@ -13,23 +13,25 @@ public class Employee {
     @Column(name = "id")
     private Long id;
 
-    @Column(name = "name")
-    private String name;
+    @Column(name = "firstName")
+    private String firstName;
 
-    @Column(name = "age")
-    private int age;
+    @Column(name = "lastName")
+    private String lastName;
 
     @Column(name = "employeeNumber")
     private int employeeNumber;
 
-    @Column(name = "email")
-    private String email;
 
-    public Employee(String name, int age, int employeeNumber, String email) {
-        this.name = name;
-        this.age = age;
+    @ManyToOne
+    @JoinColumn(name =  "department_id",nullable = false)
+    private Department department;
+
+    public Employee(String firstName, String lastName, int employeeNumber,Department department) {
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.employeeNumber = employeeNumber;
-        this.email = email;
+        this.department = department;
     }
 
     public Employee() {
@@ -43,20 +45,20 @@ public class Employee {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getFirstName() {
+        return firstName;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
-    public int getAge() {
-        return age;
+    public String getLastName() {
+        return lastName;
     }
 
-    public void setAge(int age) {
-        this.age = age;
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     public int getEmployeeNumber() {
@@ -67,11 +69,11 @@ public class Employee {
         this.employeeNumber = employeeNumber;
     }
 
-    public String getEmail() {
-        return email;
+    public Department getDepartment() {
+        return department;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public void setDepartment(Department department) {
+        this.department = department;
     }
 }
--- src/test/java/com/lab/employeeTracking/EmployeeTrackingApplicationTests.java
+++ src/test/java/com/lab/employeeTracking/EmployeeTrackingApplicationTests.java
@@ -1,6 +1,8 @@
 package com.lab.employeeTracking;
 
+import com.lab.employeeTracking.models.Department;
 import com.lab.employeeTracking.models.Employee;
+import com.lab.employeeTracking.repositories.DepartmentRepository;
 import com.lab.employeeTracking.repositories.EmployeeRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,19 +10,30 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EmployeeTrackingApplicationTests {
-	private Employee jack;
+
 
 	@Autowired
 	EmployeeRepository employeeRepository;
+	@Autowired
+	DepartmentRepository departmentRepository;
 
 	@Test
 	void contextLoads() {
 	}
 
+//	@Test
+//	public void createEmployee(){
+//		jack = new Employee("Jack",25,1001,"jack@emal.com");
+//		employeeRepository.save(jack);
+//	}
 	@Test
-	public void createEmployee(){
-		jack = new Employee("Jack",25,1001,"jack@emal.com");
+	public void createEmployeeAndDepartment(){
+		Department  department = new Department("Tech");
+		departmentRepository.save(department);
+
+		Employee jack = new Employee("Jack","smith",1001,department);
 		employeeRepository.save(jack);
+
 	}
 
 
